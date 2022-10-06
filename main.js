f2ff5e4be77c048d129b1cfee71eff5d0674f5d2
Pulkit Saxena <pulkitsaxena795@gmail.com>
2022-10-06 15:28:54 +0530

    added animation thru gsap and scrollMagic

--- main.js
+++ main.js
@@ -33,7 +33,7 @@ const btn = document.getElementById("btn");
 btn.addEventListener('click',(e) => {
     e.preventDefault();
 
-    const url = text.value;
+    const url = text.value.trim();
     // console.log(url);
 
     shortenUrl(url);
@@ -102,5 +102,172 @@ anime({
         delay: 800,
         easing: 'easeInOutQuart'
     },
+    loop: true,
     delay: 250 // All properties except 'scale' inherit 250ms delay
-  });
+});
+
+
+// TODO: There is a way to do this is in a loop but I cnt figure it out and in video it was done using Jquery
+
+// init scrollMagic
+var controller = new ScrollMagic.Controller();
+
+// create a scene
+new ScrollMagic.Scene({
+    triggerElement: ".box-1",
+    // duration: '30%', // duration doesnt mean time in scrollMagic it means px and % is for responsiveness
+    // triggerHook: 0.7
+})
+.setClassToggle(".box-1","fade-in")
+.addTo(controller)
+
+// init scrollMagic
+var controller2 = new ScrollMagic.Controller();
+
+// create a scene
+new ScrollMagic.Scene({
+    triggerElement: ".box-2",
+    // duration: '30%', // duration doesnt mean time in scrollMagic it means px and % is for responsiveness
+    // triggerHook: 0.7
+})
+.setClassToggle(".box-2","fade-in")
+.addTo(controller2)
+
+// init scrollMagic
+var controller3 = new ScrollMagic.Controller();
+
+// create a scene
+new ScrollMagic.Scene({
+    triggerElement: ".box-3",
+    // duration: '30%', // duration doesnt mean time in scrollMagic it means px and % is for responsiveness
+    // triggerHook: 0.7
+})
+.setClassToggle(".box-3","fade-in")
+.addTo(controller3)
+
+// * Parallax using SMGsap and tween
+
+// new ScrollMagic.Scene({
+//     triggerElement: '.bcg-parallax',
+//     triggerHook: 1,
+//     duration: '200%',
+// })
+// .setTween(TweenMax.from('.bcg', 1, {y: '-30%', ease: Power0.easeNone}))
+// .addTo(controller)
+
+
+// * gsap animation 
+
+// gsap.to('header div div',{
+//     duration: 1,
+//     y: 10,
+//     ease: "power2.out",
+//     repeat: -1,
+//     repeatDelay: 1,
+//     yoyo: true
+// });
+
+// gsap.from('header div div',{
+//     duration: 1,
+//     y: 10,
+//     delay: 1,
+//     ease: "power2.out",
+// });
+
+// // * animating the nav links for better visuals
+
+// gsap.fromTo(
+//     ['.link-5','.link-4','.link-3','.link-2','.link-1'],{
+//         opacity: 0,
+//         x: -10
+//     },
+//     {
+//         opacity: 1,
+//         x: 0,
+//         duration: .3,
+//         ease: 'slow(.7,.7.false)',
+//         delay: .1,
+//         stagger: .2
+//     }
+// )
+// // * animating the header content for better visuals
+
+// gsap.fromTo(
+//     ['header div div','header div div h2','header div div p','header div div a'],{
+//         opacity: 0,
+//         x: -10
+//     },
+//     {
+//         opacity: 1,
+//         x: 0,
+//         duration: .7,
+//         ease: 'slow(.7,.7.false)',
+//         delay: .7,
+//         stagger: .2
+//     }
+// )
+
+// // * animating the urlShortner section
+
+// gsap.fromTo(
+//     ['div form','.shortBtn','.urlInput'],{
+//         opacity: 0,
+//         x: -10
+//     },
+//     {
+//         opacity: 1,
+//         x: 0,
+//         duration: .5,
+//         ease: 'slow(.7,.7.false)',
+//         delay: 1.5,
+//         stagger: .2
+//     }
+// )
+
+// * creating timeline so that we dont have to manually delay each animations..since they are all linked we dont need to add delay and wait for first one to finish
+
+const timeLine = gsap.timeline();
+
+timeLine
+    .fromTo(
+            ['.link-5','.link-4','.link-3','.link-2','.link-1'],
+            {
+                opacity: 0,
+                x: -10
+            },
+            {
+                opacity: 1,
+                x: 0,
+                duration: .3,
+                ease: 'slow(.7,.7.false)',
+                stagger: .2
+            }
+        )
+    .fromTo(
+            ['header div div','header div div h2','header div div p','header div div a'],
+            {
+                opacity: 0,
+                x: -10
+            },
+            {
+                opacity: 1,
+                x: 0,
+                duration: .7,
+                ease: 'slow(.7,.7.false)',
+                stagger: .2
+            }
+        )
+    .fromTo(
+            ['div form','.shortBtn','.urlInput'],
+            {
+                opacity: 0,
+                x: -10
+            },
+            {
+                opacity: 1,
+                x: 0,
+                duration: .5,
+                ease: 'slow(.7,.7.false)',
+                stagger: .2
+            }
+        )
